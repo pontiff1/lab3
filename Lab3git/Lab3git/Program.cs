94a8222da27781b0021059d9cbf6827c2e6fa86f
pontiff <pekal2001@bk.ru>
2023-05-03 11:33:43 +0700

    menu

---
+++ Lab3git/Lab3git/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab3git
+{
+    class Proram
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Введите число а:");
+            double? a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b:");
+            double? b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("1. Сумма");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+        }
+    }
+}
