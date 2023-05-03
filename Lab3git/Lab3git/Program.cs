fb8441f99cd3f47186a840a9d54cb2a45a3eaf9b
TestAccountHelp <bluray123456789@gmail.com>
2023-05-03 11:55:44 +0700

    funcs

---
+++ Lab3git/Lab3git/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab3Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Результат: ");
+            int? c = Convert.ToInt16(Console.ReadLine());
+            switch (c)
+            {
+                case 1: plus(a, b); break;
+                case 2: minus(a, b); break;
+                case 3: multiply(a, b); break;
+                case 4: divide(a, b); break;
+            }
+        }
+        static void plus(double? x, double? y)
+        {
+            Console.WriteLine(x + y);
+        }
+        static void minus(double? x, double? y)
+        {
+            Console.WriteLine(x - y);
+        }
+        static void multiply(double? x, double? y)
+        {
+            Console.WriteLine(x * y);
+        }
+        static void divide(double? x, double? y)
+        {
+            Console.WriteLine(x / y);
+        }
+    }
+}
